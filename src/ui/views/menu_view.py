368433801a26a8affdb258532e7c25ce5c3832ba
Raphael GOISQUE <raph.goisque@gmail.com>
2026-01-02 15:20:41 +0100

    feat (Menu): button start game

--- src/ui/views/menu_view.py
+++ src/ui/views/menu_view.py
@@ -3,7 +3,7 @@ import os
 
 import arcade
 import arcade.gui
-from arcade.gui import UIAnchorLayout, UIBoxLayout
+from arcade.gui import UIAnchorLayout, UIBoxLayout, UIFlatButton, UILabel
 
 from src.ui.components.Radio import Radio
 from src.utils import conf
@@ -27,14 +27,18 @@ class MenuView(arcade.gui.UIView):
         # Layouts are containers that automatically position widgets based on the layout rules
         self.layout = root.add( UIBoxLayout( space_between=20 ) )
 
-        self.layout.add(Counter(text='Number of bots', min=0, max=30))
-        self.layout.add(Counter(text='Number of orbs', min=1, max=150))
+        self.layout.add(UILabel(text=conf['game_name']))
+        self.counter_bots = self.layout.add(Counter(text='Number of bots', min=0, max=30))
+        self.counter_orbs = self.layout.add(Counter(text='Number of orbs', min=1, max=150))
 
-        self.layout.add(Radio(
+        self.radio_game_mode = self.layout.add(Radio(
             text='Game mode :',
             options=['learn', 'I play'],
             default='learn'
         ))
+
+        self.start_button = self.layout.add(UIFlatButton(text='Start Game', width=200))
+        self.start_button.on_click = self.start_game
 
     def on_show_view(self) -> None:
 
@@ -47,14 +51,15 @@ class MenuView(arcade.gui.UIView):
     def on_draw(self):
         self.ui_manager.draw()
 
-    def on_key_press(self, key: int, modifiers: int):
+    def start_game(self, _event):
 
-        # temporary (before implementing real menu and button "play")
-        if key == arcade.key.ESCAPE:
-            world = World(nb_col=conf['grid']['nb_col'], nb_row=conf['grid']['nb_row'])
-            world.create_orbs(quantity=30)
-            world.create_snakes(quantity=3, first_is_a_player=False)
+        world = World(nb_col=conf['grid']['nb_col'], nb_row=conf['grid']['nb_row'])
+        world.create_orbs(quantity=self.counter_orbs.value)
+        world.create_snakes(
+            quantity=self.counter_bots.value,
+            first_is_a_player=self.radio_game_mode.current_value == 'I play'
+        )
+        game_view = GameView(world=world)
+        game_view.setup()
+        self.window.show_view(game_view)
 
-            game_view = GameView(world=world)
-            game_view.setup()
-            self.window.show_view(game_view)
